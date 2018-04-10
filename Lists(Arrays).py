47cc3b570b6210e80e42e15b2e351fd94cfe8cb2
sunilyekulla <38101561+sunilyekulla@users.noreply.github.com>
2018-04-10 10:48:11 +0530

    Add files via upload

---
+++ Lists(Arrays).py
@@ -0,0 +1,12 @@
+
+print(["Movies" , "Games" , "Water"][0])
+
+print(["Movies" , "Games" , "Water"][1])
+
+print(["Movies" , "Games" , "Water"][2])
+
+print("I Like " + (["Movies","Games","Water"][0]))
+
+print("I Like " + (["Movies","Games","Water"][1]))
+
+print("I Like " + (["Movies","Games","Water"][2]))
