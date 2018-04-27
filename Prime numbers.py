598d71158ee9e630f48cf1276ee85c01938d701f
sunilyekulla <38101561+sunilyekulla@users.noreply.github.com>
2018-04-27 17:33:05 +0530

    Add files via upload

---
+++ Prime numbers.py
@@ -0,0 +1,13 @@
+num1=int(input("Enter first number: "))
+num2=int(input("Enter second number: "))
+
+print("The prime numbers between",num1,"and",num2,"are:")
+
+for num in range(num1,num2 + 1):
+    if num > 1:
+        for i in range(2,num):
+            if (num % i)==0:
+                break
+
+        else:
+             print(num)
---
+++ area of rectangle.py
@@ -0,0 +1,11 @@
+length_L=int(input("Enter the length of rectangle: "))
+width_W=int(input("Enter the width of rectangle: "))
+
+#Area of rectangle
+Area=length*width
+
+#perimeter of rectangle
+Perimeter=2*length*width
+
+print("\n The area of rectangle is:", Area)
+print(" The perimeter of rectangle is:", Perimeter)
