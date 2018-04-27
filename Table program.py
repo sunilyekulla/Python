29e823f8551c0abb6660bdb1799ca51ebc0bad56
sunilyekulla <38101561+sunilyekulla@users.noreply.github.com>
2018-04-27 15:48:24 +0530

    Add files via upload

---
+++ Table program.py
@@ -0,0 +1,5 @@
+#Simple table programme
+
+n=int(input("Enter number n:"))
+for i in range(1,11):
+    print(n,'x',i,'=',n*i)
---
+++ While loop.py
@@ -0,0 +1,17 @@
+#1
+n=int(input("Enter n: "))
+sum = 0
+i=1
+
+while i <= n:
+   sum = sum + i
+   i = i+1
+print("sum is",sum)
+
+
+#2
+count =0
+while (count < 9):
+    count = count+1
+    print("Count is:",count)
+    print ("Good bye!")
